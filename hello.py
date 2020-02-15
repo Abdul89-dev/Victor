6f48b5aa53638a76a22802c53fd833ab0ba2264e
Victor Voloshin <9999822@mail.ru>
2020-02-15 18:26:42 +0300

    1 commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+print("hello world")
+print("hello programist")
+print(2 + 2)
+print(10 / 3)
---
+++ types.py
@@ -0,0 +1,59 @@
+#a = 2
+#sum = 2.5
+#b = sum - a
+#print(a + b)
+
+
+ # name = input ("input your name" ) 
+# print('Hello'+ name)
+
+
+#v = int(input("Введите число от 1 до 10")) 
+#print(v)
+
+#s = int(input("Введите число от 1 до 10")) 
+#print(s + 10)
+
+#name = input("Enter your name")
+#print(name)
+
+#print (float ('1'))
+#print(int("2.5"))
+#print(bool(1))
+#print(bool(""))
+#print(bool(0))
+
+#numbers = [3,5,7,9,10.5]
+#print(numbers)
+#numbers.append ('Python')
+#print(numbers)
+#print (len(numbers))
+
+#print(numbers[0])
+#print(numbers[-1])
+#print(numbers[2:4])
+#del numbers[-1]
+#print(numbers)
+
+#dictonary = {
+    #'city' : 'Moscow',
+    #'temperature' : '20'
+#}
+#print(dictonary ['city'])
+#print (int(dictonary['temperature']))
+#print (int(dictonary['temperature']) - 5)
+#print(type(dictonary))
+#print(dictonary)
+
+#print(dictonary.get("namecountry"))
+#print(dictonary.get("country", "Russia"))
+#dictonary['date'] = "27.05.2019"
+#print(len(dictonary))
+
+#def get_sum(one, two, delimiter='&'):
+    #if one == "Learn" and two =="python":
+        #return(one + delimiter + two)
+        
+#print(get_sum('Learnl', 'python'))
+
+
